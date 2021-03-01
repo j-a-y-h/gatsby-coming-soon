ebbed12af465c971f51bd293af1c8b5b01e0fc56
jay <jay@sourware.com>
2021-02-28 23:05:25 -0500

    Adds billboard to home page

--- src/components/Billboard.tsx
+++ src/components/Billboard.tsx
@@ -2,17 +2,13 @@ import React from "react";
 import { useWindowSize } from "../hooks";
 
 type Props = Readonly<{
-  subjectRef?: string;
-  y: number;
   image: string;
-  title: React.ReactChildren;
-  children: React.ReactChildren;
-  setHeight: number;
+  title: string;
+  children: JSX.Element;
+  setHeight?: number;
 }>;
 
 export default function Billboard({
-  y = 0,
-  subjectRef,
   image,
   title,
   children,
@@ -22,35 +18,34 @@ export default function Billboard({
   return (
     <div
       className="jumbotron"
-      ref={subjectRef}
       style={{
         background: `center / cover no-repeat url('${image}')`,
-        backgroundPositionY: `${y * 2}px`,
         height: `${setHeight || height || 360}px`,
         position: "relative",
       }}
     >
+      <div style={{
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        position: 'absolute',
+        backgroundColor: 'rgba(0,0,0,0.5)'
+      }}></div>
       <div
-        className="container-fluid text-center"
+        className="container-fluid"
         style={{
           position: "absolute",
-          bottom: 0,
+          top: 0,
           color: "white",
           right: 0,
           left: 0,
-          paddingBottom: "20px",
-          background:
-            "linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0.6965161064425771) 50%, rgba(0,0,0,0) 100%)",
+          padding: "60px",
+          zIndex: 1,
         }}
       >
         <h1 style={{ color: "white" }}>{title}</h1>
         {children}
-        <p
-          className="text-small"
-          style={{ fontSize: "12px", fontStyle: "italic", color: "rgba(255,255,255,0.75)" }}
-        >
-          Scroll to view more
-        </p>
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,19 @@
 import * as React from "react"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { } from 'bootstrap';
+import Billboard from "../components/Billboard";
 
 const data = {
   name: 'Company',
   phone: '555-555-5555',
-  email: 'contact@example.com'
+  email: 'contact@example.com',
+  message: 'Lorem ipsum dolor sit amet consectetur adipisicing elit sed eiu sit amet consectetur'
 }
 
 // markup
 const IndexPage = () => {
   return (
-    <main className="container-fluid">
-      <nav className="navbar navbar-light bg-light">
+    <main>
+      <nav className="navbar navbar-dark bg-dark fixed-top">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
             <img src="https://beta.sourware.com/img/sourware_o.png" alt="Logo" height="24" className="d-inline-block align-top" />
@@ -23,6 +24,15 @@ const IndexPage = () => {
           </div>
         </div>
       </nav>
+      <Billboard image="/img/laptop-1920.jpg" title="Test">
+        <>
+          <p>{data.message}</p>
+          <div className="input-group mb-3">
+            <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
+            <button className="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+          </div>
+        </>
+      </Billboard>
     </main>
   )
 }
