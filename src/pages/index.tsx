7b9a9efdb08433ac84e61dc8ed215462dafb1d29
jay <jay@sourware.com>
2021-03-01 10:32:33 -0500

    Adds count down and popping font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import Helmet from 'react-helmet'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Billboard from "../components/Billboard";
 
@@ -12,6 +13,10 @@ const data = {
 // markup
 const IndexPage = () => {
   return (
+    <>
+    <Helmet>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins" />
+    </Helmet>
     <main>
       <nav className="navbar navbar-dark bg-dark fixed-top">
         <div className="container-fluid">
@@ -24,16 +29,41 @@ const IndexPage = () => {
           </div>
         </div>
       </nav>
-      <Billboard image="/img/laptop-1920.jpg" title="Test">
+      <Billboard image="/img/laptop-1920.jpg" title="We are coming soon!">
         <>
-          <p>{data.message}</p>
-          <div className="input-group mb-3">
-            <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
-            <button className="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+          <div className="row">
+            <div className="col-6">
+              <div className="row text-center">
+                <div className="col-md">
+                  <p>Days</p>
+                  <p style={{fontSize: '4rem'}}><strong>63</strong></p>
+                </div>
+                <div className="col-md">
+                  <p>Hours</p>
+                  <p>63</p>
+                </div>
+                <div className="col-md">
+                  <p>Minutes</p>
+                  <p>63</p>
+                </div>
+                <div className="col-md">
+                  <p>Seconds</p>
+                  <p>63</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <p>{data.message}</p>
+              <div className="input-group mb-3">
+                <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                <button className="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+              </div>  
+            </div>
           </div>
         </>
       </Billboard>
     </main>
+    </>
   )
 }
 
