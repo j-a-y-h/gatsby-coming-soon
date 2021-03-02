2d108f11054b59c1fd41f137b9cebf422f7dd408
jay <jay@sourware.com>
2021-03-02 15:48:20 -0500

    Adds social media links

--- src/components/SocialMediaIcons.tsx
+++ src/components/SocialMediaIcons.tsx
@@ -3,26 +3,32 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircle } from '@fortawesome/free-solid-svg-icons'
 import { faInstagram, faFacebookF , faTwitter } from '@fortawesome/free-brands-svg-icons'
 
-const map = {
+const iconMap = {
   "Instagram": faInstagram,
   "Facebook": faFacebookF,
   "Twitter": faTwitter,
 }
 
+const urlMap = {
+  "Instagram": "https://www.instagram.com",
+  "Facebook": "https://www.facebook.com",
+  "Twitter": "https://twitter.com",
+}
+
 export type Props = Readonly<{
-  icons: string[];
+  icons: string[][];
 }>
 
 export default function SocialMediaIcons ({icons}: Props) {
   return (
     <>
       <ul className="nav justify-content-center">
-        {icons.map((name) => (
+        {icons.map(([name, username]) => (
           <li className="nav-item text-center">
-            <a className="nav-link" href="#">
+            <a className="nav-link" rel="noopener nofollow" target="_blank" href={`${urlMap[name]}/${username}`}>
               <span className="fa-layers fa-fw fa-3x text-center">
-                <FontAwesomeIcon icon={faCircle}   />
-                <FontAwesomeIcon icon={map[name]}  inverse color="white" transform="shrink-8" />
+                <FontAwesomeIcon icon={faCircle} />
+                <FontAwesomeIcon icon={iconMap[name]}  inverse color="white" transform="shrink-8" />
               </span>
             </a>
           </li>
