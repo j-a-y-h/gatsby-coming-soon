1a8428429ab55da4f827085cd7376b463b39be66
jay <jay@sourware.com>
2021-03-02 13:17:55 -0500

    Moves data to separate file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,15 +6,7 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Countdown from "../components/Countdown";
 import { AnimatedText } from "../components/AnimatedText";
-
-const data = {
-  name: 'Company',
-  phone: '(555) 555-5555',
-  email: 'contact@example.com',
-  launchDate: '4/1/2021',
-  icons: ['Instagram', 'Facebook', 'Twitter'],
-  message: "Our amazing website is currently under construction and we can't wait for you to experience it! Please enter your email below for launch notification and/or beta testing instructions."
-}
+import data from "../about"
 
 // markup
 const IndexPage = () => {
