82551ce34a4a1af0969ad3372377e9bd1fb0d385
jay <jay@sourware.com>
2021-03-02 13:56:34 -0500

    Fixes metadata and favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ const about = require("./src/about.json");
 
 module.exports = {
   siteMetadata: {
-    title: `${about.name} | Coming Soon`,
+    title: `Coming Soon`,
     siteUrl: `https://www.sourware.com`,
     description: `Blazing fast modern site generator for React`,
   },
@@ -21,7 +21,19 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/icon.png",
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: about.background_color,
+        theme_color: about.theme_color,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "browser",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
       },
     },
     "gatsby-transformer-sharp",
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ const IndexPage = () => {
   return (
     <>
     <Helmet>
+      <title>{data.name} | Coming Soon</title>
       <link 
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" 
         rel="stylesheet" 
