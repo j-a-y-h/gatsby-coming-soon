9fee3275d859df6e2eb00562898d79908e045777
jay <jay@sourware.com>
2021-03-02 14:38:44 -0500

    Adds validation to email form

--- src/components/Subscribe.tsx
+++ src/components/Subscribe.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import {TextField} from "@material-ui/core"
 import data from "../about.json"
 import moment from "moment";
+import {useForm, useSettersAsEventHandler} from "react-uniformed"
 
 type Props = Readonly<{
   launchDate: string;
@@ -9,16 +10,24 @@ type Props = Readonly<{
 
 export default function Subscribe({launchDate}: Props) {
   const parsedMessage = data.message.replace("<date>", moment(launchDate).format('dddd, MMMM Do'));
+  const {hasErrors, isDirty, isSubmitting, validateByName, touchField} = useForm({
+    onSubmit: () => {},
+    constraints: {
+      email: { type: 'email' }
+    }
+  })
+  const onChange = useSettersAsEventHandler(validateByName, touchField);
   return (
     <>
       <p>{parsedMessage}</p>
       <div className="input-group mb-3">
         <TextField
-          error
-          id="outlined-error"
+          error={hasErrors}
           label="Enter email address"
           variant="filled"
           type="email"
+          name="email"
+          onChange={onChange}
           style={{
             position: "relative",
             flex: "1 1 auto",
@@ -33,7 +42,12 @@ export default function Subscribe({launchDate}: Props) {
             "aria-describedby": "button-addon2"
           }}
         />
-        <button className="btn btn-primary" type="button" id="button-addon2">Notify Me</button>
+        <button 
+          disabled={!isDirty || hasErrors || isSubmitting} 
+          className="btn btn-primary" 
+          type="button" 
+          id="button-addon2"
+        >Notify Me</button>
       </div>  
     </>
   )
