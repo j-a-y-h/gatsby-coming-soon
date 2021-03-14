e908032b93cbdda65c1b68bd45dbd2af31078fc0
jay <jay@sourware.com>
2021-03-13 20:34:47 -0500

    Adds the ability to send request

--- src/components/Subscribe.tsx
+++ src/components/Subscribe.tsx
@@ -10,13 +10,27 @@ type Props = Readonly<{
 
 export default function Subscribe({launchDate}: Props) {
   const parsedMessage = data.message.replace("<date>", moment(launchDate).format('dddd, MMMM Do'));
-  const {hasErrors, isDirty, isSubmitting, validateByName, touchField} = useForm({
-    onSubmit: () => {},
+  const {hasErrors, isDirty, isSubmitting, validateByName, touchField, submit, setValue} = useForm({
+    onSubmit(values, api) {
+      // TODO: move to config
+      fetch("http://localhost:3000/subscribe-launch-notification/", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(values)
+      }).then(() => {
+        api.setFeedback
+      }, (e) => {
+        console.log(e);
+        api.setError("email", "Sorry, something went wrong.");
+      })
+    },
     constraints: {
       email: { type: 'email' }
     }
   })
-  const onChange = useSettersAsEventHandler(validateByName, touchField);
+  const onChange = useSettersAsEventHandler(validateByName, touchField, setValue);
   return (
     <>
       <p>{parsedMessage}</p>
@@ -46,6 +60,7 @@ export default function Subscribe({launchDate}: Props) {
           disabled={!isDirty || hasErrors || isSubmitting} 
           className="btn btn-primary" 
           type="button" 
+          onClick={submit}
           id="button-addon2"
         >Notify Me</button>
       </div>  
