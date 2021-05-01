fe1def7e02799882fd548352b2d47004893749d5
jay <jay@sourware.com>
2021-05-01 16:19:09 -0400

    Adds support for custom theme

--- src/components/SocialMediaIcons.tsx
+++ src/components/SocialMediaIcons.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircle } from '@fortawesome/free-solid-svg-icons'
 import { faInstagram, faFacebookF , faTwitter } from '@fortawesome/free-brands-svg-icons'
+import about from "../about.json";
 
 const iconMap = {
   "Instagram": faInstagram,
@@ -27,7 +28,7 @@ export default function SocialMediaIcons ({icons}: Props) {
           <li className="nav-item text-center">
             <a className="nav-link" rel="noopener nofollow" target="_blank" href={`${urlMap[name]}/${username}`}>
               <span className="fa-layers fa-fw fa-3x text-center">
-                <FontAwesomeIcon icon={faCircle} />
+                <FontAwesomeIcon icon={faCircle} color={about.primaryColor} />
                 <FontAwesomeIcon icon={iconMap[name]}  inverse color="white" transform="shrink-8" />
               </span>
             </a>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,49 @@ import data from "../about.json"
 import Head from "../components/Head";
 import MainContent from "../components/MainContent";
 
+function LightenDarkenColor(col, amt) {
+  
+  var usePound = false;
+
+  if (col[0] == "#") {
+      col = col.slice(1);
+      usePound = true;
+  }
+
+  var num = parseInt(col,16);
+
+  var r = (num >> 16) + amt;
+
+  if (r > 255) r = 255;
+  else if  (r < 0) r = 0;
+
+  var b = ((num >> 8) & 0x00FF) + amt;
+
+  if (b > 255) b = 255;
+  else if  (b < 0) b = 0;
+
+  var g = (num & 0x0000FF) + amt;
+
+  if (g > 255) g = 255;
+  else if (g < 0) g = 0;
+
+  return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+
+}
 function CustomStyles() {
   return (<style>{`
+      .btn-primary {
+        background-color: ${data.primaryColor} !important;
+        border-color: ${LightenDarkenColor(data.primaryColor, -20)} !important;
+      }
+      a {
+        color: ${data.primaryColor};
+        text-decoration: none;
+      }
+      a:hover {
+        color: ${data.primaryColor};
+        text-decoration: underline;
+      }
       @media (min-width: 576px) {
       .mt-sm-5 {
           margin-top: 8rem!important;
